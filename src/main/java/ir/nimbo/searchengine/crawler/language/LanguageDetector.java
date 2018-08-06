83f4b84916917768b53ca73931d2180875ff46d5
mohammadreza <mohammadreza.salehi951@gmail.com>
2018-08-06 18:45:05 +0430

    conflicts merged

--- src/main/java/ir/nimbo/searchengine/crawler/language/LanguageDetector.java
+++ src/main/java/ir/nimbo/searchengine/crawler/language/LanguageDetector.java
@@ -3,19 +3,18 @@ package ir.nimbo.searchengine.crawler.language;
 import com.cybozu.labs.langdetect.Detector;
 import com.cybozu.labs.langdetect.DetectorFactory;
 import com.cybozu.labs.langdetect.LangDetectException;
-import ir.nimbo.searchengine.crawler.Link;
 import ir.nimbo.searchengine.exception.IllegalLanguageException;
 
-import java.util.LinkedHashSet;
 
 public class LanguageDetector {
-   public static void profileLoad(String address){
-       try {
-           DetectorFactory.loadProfile(address);
-       } catch (LangDetectException e) {
-           e.printStackTrace();
-       }
-   }
+    public static void profileLoad(String address) {
+        try {
+            DetectorFactory.loadProfile(address);
+        } catch (LangDetectException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void languageCheck(String text) throws IllegalLanguageException {
         Detector detector;
         try {
