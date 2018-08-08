5d0ebb698d1956a05419ae349ade66383ce4444c
mohammadreza <mohammadreza.salehi951@gmail.com>
2018-08-08 13:49:48 +0430

    duplicate Check Complete

--- src/test/java/ir/nimbo/searchengine/crawler/DuplicateLinkHandlerTest.java
+++ src/test/java/ir/nimbo/searchengine/crawler/DuplicateLinkHandlerTest.java
@@ -16,6 +16,8 @@ public class DuplicateLinkHandlerTest {
 
     @Test
     public void loadHashTable() {
+        DuplicateLinkHandler.getInstance().isDuplicate("salam");
+        DuplicateLinkHandler.getInstance().saveHashTable();
         DuplicateLinkHandler.getInstance().loadHashTable();
         Assert.assertTrue(DuplicateLinkHandler.getInstance().isDuplicate("salam"));
         Assert.assertFalse(DuplicateLinkHandler.getInstance().isDuplicate(" che khabar"));
